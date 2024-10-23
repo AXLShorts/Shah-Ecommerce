684140fca0acf7ad6983b21c57cfe99e79f0081e
Farrukh-Mustafa <farukh.mustafa@buggcy.com>
2024-10-23 09:11:28 +0500

    Homepage

---
+++ app/page.tsx
@@ -0,0 +1,152 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+const products = [
+  {
+    id: 1,
+    img: "https://picsum.photos/400/200",
+    title: "Energy Audit",
+    price: 300,
+  },
+  {
+    id: 2,
+    img: "https://picsum.photos/400/200",
+    title: "Grant Consultation 1 Hour",
+    price: 150,
+  },
+  {
+    id: 3,
+    img: "https://picsum.photos/400/200",
+    title: "New Services",
+    price: 250,
+  },
+  {
+    id: 4,
+    img: "https://picsum.photos/400/200",
+    title: "Cancel Service",
+    price: 200,
+  },
+  {
+    id: 5,
+    img: "https://picsum.photos/400/200",
+    title: "Technician Site Visit",
+    price: 100,
+  },
+  {
+    id: 6,
+    img: "https://picsum.photos/400/200",
+    title: "Service Relocation",
+    price: 150,
+  },
+  {
+    id: 7,
+    img: "https://picsum.photos/400/200",
+    title: "Reconnect Service",
+    price: 50,
+  },
+  {
+    id: 8,
+    img: "https://picsum.photos/400/200",
+    title: "Service Upgrade",
+    price: 100,
+  },
+  {
+    id: 9,
+    img: "https://picsum.photos/400/200",
+    title: "Solar Panel Consultation (Call)",
+    price: "Free",
+  },
+  {
+    id: 10,
+    img: "https://picsum.photos/400/200",
+    title: "Solar Panel Consultation (Site Visit)",
+    price: 200,
+  },
+  {
+    id: 11,
+    img: "https://picsum.photos/400/200",
+    title: "Insulation Consultation",
+    price: "Free",
+  },
+  {
+    id: 12,
+    img: "https://picsum.photos/400/200",
+    title: "Insulation Consultation (Site Visit)",
+    price: 200,
+  },
+  {
+    id: 13,
+    img: "https://picsum.photos/400/200",
+    title: "Solar Panel Installation",
+    price: "Contact for Quote",
+  },
+  {
+    id: 14,
+    img: "https://picsum.photos/400/200",
+    title: "Insulation Upgrade",
+    price: "Contact for Quote",
+  },
+  {
+    id: 15,
+    img: "https://picsum.photos/400/200",
+    title: "Furnace Upgrade",
+    price: "1,000+ Material cost",
+  },
+  {
+    id: 16,
+    img: "https://picsum.photos/400/200",
+    title: "Electric Vehicle Charger",
+    price: 2000,
+  },
+];
+
+export default function Home() {
+  return (
+    <div className="p-4 py-0 md:py-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {products.map((product) => (
+          <Card
+            key={product.id}
+            className="flex flex-col justify-between relative"
+          >
+            <div className="absolute right-2 top-2 text-[#212529] rounded-xl px-2.5 py-1 bg-[#e6ebef] text-sm flex justify-center items-center">
+              {typeof product.price === "number"
+                ? `$${product.price}`
+                : product.price}
+            </div>
+            <CardHeader className="p-0 pb-4 rounded-lg rounded-b-none overflow-hidden">
+              <Image
+                src={product.img}
+                alt={product.title}
+                width={400}
+                height={200}
+                className="w-full"
+              />
+            </CardHeader>
+            <CardContent className="flex-grow flex flex-col justify-start p-4 pt-0">
+              <CardTitle className="text-lg">{product.title}</CardTitle>
+            </CardContent>
+            <CardFooter className="mt-auto p-4 pt-0">
+              <Button
+                className="w-full text-white bg-[#31a08a]"
+                variant="default"
+              >
+                Book Now
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { Menu } from "lucide-react";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[80px] flex justify-between items-center text-nowrap px-4">
+      <div>
+        <Link href="/" className="flex gap-2 sm:gap-4 items-center">
+          <Image
+            src="/assets/logo.png"
+            alt="Logo"
+            width={40}
+            height={40}
+            className="cursor-pointer"
+          />
+          <h1 className="text-[#18571b] text-lg sm:text-2xl font-bold">
+            NA Energy Solutions
+          </h1>
+        </Link>
+      </div>
+      <div className="lg:flex gap-2 xl:gap-4 w-1/2 items-center justify-end hidden">
+        <Link href="/">
+          <Button variant="link" className="text-base px-2 xl:px-4">
+            Home
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button variant="link" className="text-base px-2 xl:px-4">
+            About Us
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button variant="link" className="text-base px-2 xl:px-4">
+            Contact
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button variant="link" className="text-base px-2 xl:px-4">
+            Privacy Policy
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button variant="link" className="text-base px-2 xl:px-4">
+            Refund Policy
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button variant="link" className="text-base px-2 xl:px-4">
+            Terms & Conditions
+          </Button>
+        </Link>
+      </div>
+      <div className="flex lg:hidden">
+        <Sheet>
+          <SheetTrigger className="p-1 border rounded-md">
+            <Menu size={24} />
+          </SheetTrigger>
+          <SheetContent side={"right"}>
+            <SheetHeader className="pt-6">
+              <SheetTitle className="hidden">Menu</SheetTitle>
+              <Button variant="default">Home</Button>
+              <Button variant="default">About Us</Button>
+              <Button variant="default">Contact</Button>
+              <Button variant="default">Privacy Policy</Button>
+              <Button variant="default">Refund Policy</Button>
+              <Button variant="default">Terms & Conditions</Button>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
